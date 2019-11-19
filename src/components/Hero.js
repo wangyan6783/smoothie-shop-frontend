7ea509e5f0c0854979127e10dbc12fce3976a839
github username <wangyan6783@gmail.com>
2019-11-18 22:36:55 -0600

    hero component styling

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,10 +5,11 @@ class Hero extends Component {
     render(){
         return (
             <div className="hero">
-                <img src={heroImg} alt="smoothie shop hero image" />
-                <h1>Eat more fruites + vegetables</h1>
-                <p>We take care of food so food can take care of you.</p>
-                <button>Get started</button>
+                <div className="hero__container">
+                    <h1 className="title-primary hero__title">Eat more fruites + vegetables</h1>
+                    <p className=" content-primary hero__content">We take care of food so food can take care of you.</p>
+                    <button className="btn hero__btn">Get started</button>
+                </div>
             </div>
         )
     }
