4899dc2001ea834b20fdd0a907dfd4d5f5e6b5c3
github username <wangyan6783@gmail.com>
2019-11-19 21:23:23 -0600

    hero button

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,8 +6,8 @@ class Hero extends Component {
         return (
             <div className="hero">
                 <div className="hero__container">
-                    <h1 className="title-primary hero__title">Eat more fruites + vegetables</h1>
-                    <p className=" content-primary hero__content">We take care of food so food can take care of you.</p>
+                    <h1 className="title-primary hero__title mb-sm">Eat more fruites + vegetables</h1>
+                    <p className=" content-primary mb-md">We take care of food so food can take care of you.</p>
                     <button className="btn hero__btn">Get started</button>
                 </div>
             </div>
