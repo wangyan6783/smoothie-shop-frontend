803c63cd357b0f3217dcc53d9c34dc9d09a88992
Yan <wangyan6783@gmail.com>
2020-02-21 15:20:43 -0600

    form styling

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -40,13 +40,17 @@ class Login extends Component {
         return (
             <div className="login">
                 <form className="form" onSubmit={this.handleSubmit}>
-                    <h1 className="heading-1 mb-md form__title">Log in</h1>
-                    <label className="form__label" htmlFor="username">Username</label>
-                    <input className="form__input" type="text" name="username" onChange={this.handleUsername}></input>
-                    <label className="form__label" htmlFor="password">Password</label>
-                    <input className="form__input mb-sm" type="text" name="password" onChange={this.handlePassword}></input>
-                    <button className="btn form__btn mb-sm" type="submit">Log in</button>
-                    <p className="form__text">Don't have an account? <Link className="form__link" to="/signup">Sign Up</Link></p>
+                    <h1 className="heading-1 mb-md">Log in</h1>
+                    <div className="form-field">
+                        <label className="form-text" htmlFor="username">Username</label>
+                        <input className="form-input" type="text" name="username" onChange={this.handleUsername}></input>
+                    </div>
+                    <div className="form-field">
+                        <label className="form-text" htmlFor="password">Password</label>
+                        <input className="form-input mb-sm" type="text" name="password" onChange={this.handlePassword}></input>
+                    </div>
+                    <button className="btn form-btn mb-sm" type="submit">Log in</button>
+                    <p className="form-text">Don't have an account? <Link className="form-link" to="/signup">Sign Up</Link></p>
                 </form>
             </div>
         )
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -40,13 +40,17 @@ class Signup extends Component {
         return (
             <div className="signup">
                 <form className="form" onSubmit={this.handleSubmit}>
-                    <h1 className="heading-1 mb-md form__title">Sign up</h1>
-                    <label className="form__label" htmlFor="username">Username</label>
-                    <input className="form__input" type="text" name="username" onChange={this.handleUsername}></input>
-                    <label className="form__label" htmlFor="password">Password</label>
-                    <input className="form__input mb-sm" type="text" name="password" onChange={this.handlePassword}></input>
-                    <button className="btn form__btn mb-sm" type="submit">Sign up</button>
-                    <p className="form__text">Already have an account? <Link className="form__link" to="/login">Log In</Link></p>
+                    <h1 className="heading-1 mb-md">Sign up</h1>
+                    <div className="form-field">
+                        <label className="form-text" htmlFor="username">Username</label>
+                        <input className="form-input" type="text" name="username" onChange={this.handleUsername}></input>
+                    </div>
+                    <div className="form-field">
+                        <label className="form-text" htmlFor="password">Password</label>
+                        <input className="form-input mb-sm" type="text" name="password" onChange={this.handlePassword}></input>
+                    </div>
+                    <button className="btn form-btn mb-sm" type="submit">Sign up</button>
+                    <p className="form-text">Already have an account? <Link className="form-link" to="/login">Log In</Link></p>
                 </form>
             </div>
         )
