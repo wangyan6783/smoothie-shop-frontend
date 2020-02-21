ef5e7b8febfa98bb097727156d4cceb7f5ba1189
Yan <wangyan6783@gmail.com>
2020-02-21 13:46:14 -0600

    navbar styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,17 +4,17 @@ import { Link } from 'react-router-dom';
 class Navbar extends Component {
     render(){
         return (
-            <div className="navbar">
-                <ul className="navbar__list">
-                    <li className="navbar__item"><Link className="navbar__link" to="/smoothies">Browse</Link></li>
-                    <li className="navbar__item"><Link className="navbar__link" to="/">Our Story</Link></li>
-                    <li className="navbar__item"><Link className="navbar__link" to="/">Gifts</Link></li>
-                    <li className="navbar__item navbar__item--logo"><Link className="navbar__logo" to="/">Daily Harvest</Link></li>
-                    <li className="navbar__item"><Link className="navbar__link" to="/">Help</Link></li>
-                    <li className="navbar__item"><Link className="navbar__link" to="/login">Login</Link></li>
-                    <li className="navbar__item"><Link className="navbar__link" to="/">Get Started</Link></li>
+            <nav className="navbar">
+                <ul className="navbar-list">
+                    <li className="navbar-item"><Link className="navbar-link" to="/smoothies">Browse</Link></li>
+                    <li className="navbar-item"><Link className="navbar-link" to="/">Our Story</Link></li>
+                    <li className="navbar-item"><Link className="navbar-link" to="/">Gifts</Link></li>
+                    <li className="navbar-item"><Link className="navbar-logo" to="/">Daily Harvest</Link></li>
+                    <li className="navbar-item"><Link className="navbar-link" to="/">Help</Link></li>
+                    <li className="navbar-item"><Link className="navbar-link" to="/login">Login</Link></li>
+                    <li className="navbar-item"><Link className="navbar-link" to="/">Get Started</Link></li>
                 </ul>
-            </div>
+            </nav>
         )
     }
 }
