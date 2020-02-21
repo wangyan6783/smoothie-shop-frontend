6cb4c70fb95256249f3967a9eb4ebf8a7ddefacf
Yan <wangyan6783@gmail.com>
2020-02-21 16:42:18 -0600

    smoothie modal styling

--- src/components/SmoothieModal.js
+++ src/components/SmoothieModal.js
@@ -13,17 +13,17 @@ class SmoothieModal extends Component {
         const { smoothie } = this.props;
         return (
             <div className="modal" id="smoothie-modal" onClick={this.closeModal}>
-                <div className="modal__content-container">
-                    <div className="modal__close"><span id="smoothie-close">x</span></div>
-                    <div className="modal__contents">
-                        <div className="modal__image-container">
-                            <img src={require(`../assets/smoothies/${smoothie.imgUrl}-1.jpeg`)} alt={smoothie.name} className="modal__image"/>
+                <div className="modal-content-container">
+                    <div className="modal-close"><span id="smoothie-close">x</span></div>
+                    <div className="modal-content">
+                        <div className="modal-slot-1">
+                            <img src={require(`../assets/smoothies/${smoothie.imgUrl}-1.jpeg`)} alt={smoothie.name} className="modal-image"/>
                         </div>
-                        <div className="modal__text-container">
-                            <div className="modal__title heading-2 mb-sm">{smoothie.name}</div>
-                            <div className="modal__subtitle content-text-1 mb-sm">Tastes Like: {smoothie.tastes}</div>
-                            <p className="modal__description content-text-2 mb-md">{smoothie.description}</p>
-                            <button className="btn modal__button">Add to cart</button>
+                        <div className="modal-slot-2">
+                            <div className="modal-title heading-2 mb-sm">{smoothie.name}</div>
+                            <div className="modal-subtitle content-text-1 mb-sm">Tastes Like: {smoothie.tastes}</div>
+                            <p className="modal-description content-text-2 mb-md">{smoothie.description}</p>
+                            <button className="btn modal-button">Add to cart</button>
                         </div>
                     </div>
                 </div>
